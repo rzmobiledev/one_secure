22932d6a75cefee023501b436852caed7ecd8fea
safrizal <rzmobiledev@gmail.com>
2025-06-19 09:59:40 +0700

    modify dashboard page (add notes to mail echo result)

--- frontend/src/dashboard/dashboard.tsx
+++ frontend/src/dashboard/dashboard.tsx
@@ -98,6 +98,40 @@ function Dashboard() {
                     <p style={{ textAlign: "left", paddingTop: 20}}>{result ? result : "Result of checking domain will display here..."}</p>
                 </div>
             </div>
+            {result ? (
+                <div className="chart-container" style={{marginTop: 15}}>
+                    <h3>Output</h3>
+                    <div id="chart">
+                        <div style={{ textAlign: "left", paddingTop: 20}}>
+                        <ul dir="auto">
+                            <li>
+                            <p dir="auto">NO MX Record for the domain : In this case the domain is not used for email purposes. For such domains the ideal value of SPF and DMARC for spoofing prevention should be :</p>
+                            <ul dir="auto">
+                            <li>"v=spfv1 -all" : This ensures that no server can send email for the domain.</li>
+                            <li>"v=DMARC1; p=reject; rua=<a href="mailto:admin@domain.tld">mailto:admin@domain.tld</a>" : Mail is rejected on receiving end and report is generated and sent to email addreess specified.</li>
+                            <li>DKIM for such domain is not required as it won't be used in communication</li>
+                            </ul>
+                            </li>
+                            <li>
+                            <p dir="auto">MX Record is valid: In this case check for SPF and DMARC values for predicting if the mail can be spoofed.</p>
+                            <ul dir="auto">
+                            <li>Check for SPF issues like more than 10 lookups, void lookups and syntax errors</li>
+                            <li>The DMARC value can be following:
+                            <ul dir="auto">
+                            <li>p=none : This mode means the DMARC is in monitoring mode. If the SPF and DKIM fails, reports would be generated email specified in record. Spoofing is possible here.</li>
+                            <li>p=quarantine : If DKIM and SPF fails, mail goes to spam folder.</li>
+                            <li>p=reject : This is the ideal value that should be opted for best security. The email would be rejected and report would be generated and sent to specified email.</li>
+                            </ul>
+                            </li>
+                            <li>DKIM field may be verified using the selectors. If the script is unable to find DKIM, use custom selectors or find selector using email header analysis of emails received from the people of that domain.</li>
+                            </ul>
+                            </li>
+                        </ul>
+                        </div>
+                    
+                    </div>
+                </div>
+            ): ''}
         </div>
     </div>
   )
