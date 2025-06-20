7e3ef44efe8f4829eb9408820e6060435ba98281
safrizal <rzmobiledev@gmail.com>
2025-06-20 19:20:09 +0700

    update aumation.sh

--- backend/scripts/automation.sh
+++ backend/scripts/automation.sh
@@ -3,7 +3,7 @@
 set -e
 /usr/local/bin/wait-for-it.sh db:5432
 npx prisma db push
-npm run start
 echo "================================="
 echo "YOUR SERVER IS UP AND RUNNING NOW"
 echo "================================="
+npm run start
